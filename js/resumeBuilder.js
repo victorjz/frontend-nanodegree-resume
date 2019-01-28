84f7f57f2f71cedb434b890ac64388f2138c336a
victorjz <vjzara@gmail.com>
2019-01-27 16:12:56 -0800

    added resume json data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,78 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+    "jobs":[
+        {
+            "employer": "Frontier",
+            "title": "sys admin",
+            "location": "Gresham, OR",
+            "dates": "Apr 2016 - Present",
+            "description": "troubleshoot and extend application"
+        },
+        {
+            "employer": "Verizon",
+            "title": "contract admin",
+            "location": "Thousand Oaks, CA",
+            "dates": "Dec 2004 - Apr 2016",
+            "description": "inform employees on proper use of contractor resources"
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+       {
+          "title": "cat-a-log",
+          "dates": "Oct 2012",
+          "description": "database of items with categories and images",
+          "images": [
+             "http://www.gettyimages.com/detail/978043334"
+          ]
+       }
+    ]
+ }
+
+var bio = {
+    "name": "Victor Z",
+    "role": "developer",
+    "welcomeMessage": "Hello, World",
+    "biopic": "Looking for a coding job",
+    "contacts": {
+       "mobile": "555-555-1234",
+       "email": "me@fake.com",
+       "github": "gitUser",
+       "location": "Portland, OR"
+    },
+    "skills": [
+       "HTML",
+       "JavaScript",
+       "JSON"
+    ]
+ }
+
+var education = {
+    "schools": [
+       {
+          "name": "ucla",
+          "location": "Los Angeles, CA",
+          "degree": "BS",
+          "dates": "2000 - 2004",
+          "url": "ucla.edu",
+          "majors": [
+             "cs"
+          ]
+       }
+    ],
+    "onlineCourses": [
+       {
+          "title": "software as a service",
+          "school": "edx",
+          "dates": "2012",
+          "url": "edx.com"
+       },
+       {
+          "title": "fullstack nanodegree",
+          "school": "udacity",
+          "dates": "2016",
+          "url": "udacity.com"
+       }
+    ]
+ }
